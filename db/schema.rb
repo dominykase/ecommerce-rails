d9b43a5a3b95a4fccd42aecea9c0bb18513ddb6b
Dominykas <dominykas.eicinas@gmail.com>
2024-06-01 20:25:13 +0300

    install packwerk, move users to separate package (not working)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2024_06_01_092046) do
-  create_table "items", charset: "utf8mb4", force: :cascade do |t|
+  create_table "items", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.float "price"
     t.float "discount"
@@ -19,7 +19,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_01_092046) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", charset: "utf8mb4", force: :cascade do |t|
+  create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "login"
     t.string "password_digest"
     t.datetime "created_at", null: false
